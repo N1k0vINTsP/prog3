4f3e40e89b97582666305473dbf28c2063355e43
Vince N Perez C <141367327+N1k0vINTsP@users.noreply.github.com>
2023-08-19 20:24:06 -0500

    Update Quiz1_1.java

--- quices/Quiz1_1.java
+++ quices/Quiz1_1.java
@@ -1,1 +1,44 @@
 
+import java.util.Scanner;
+
+public class Quiz1_1{
+    
+    public static void main(String[] args){
+        
+        Scanner escribir = new Scanner(System.in);
+        
+        int n = escribir.nextInt();
+        
+        for(int i=0; i<n;i++){
+           
+           if((i+1)%3 == 0 && (i+1)%5 == 0){
+               
+               System.out.println("fizzbuzz");
+               
+           }else{
+               
+               if((i+1)%5 == 0){
+                   
+                   System.out.println("buzz");
+                   
+               }else{
+                   
+                   if((i+1)%3 == 0){
+                       
+                       System.out.println("fizz");
+                       
+                   }else{
+                       
+                       System.out.println(i+1);
+                       
+                   }
+               }
+               
+           }
+            
+        }
+        
+        escribir.close();
+    }
+    
+}
