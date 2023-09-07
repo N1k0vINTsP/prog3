ebcded3440d7bdba6f00d45a648dfa7b3476147c
Vince N Perez C <141367327+N1k0vINTsP@users.noreply.github.com>
2023-09-07 01:51:16 -0500

    Add files via upload

---
+++ labs/02/OperacionMatrices/labs/02/OperacionMatrices/src/main/java/com/mycompany/operacionmatrices/OperacionMatrices.java
@@ -0,0 +1,26 @@
+package com.mycompany.operacionmatrices;
+
+import java.util.Scanner;
+
+
+
+
+public class OperacionMatrices{
+    
+    public static double[][] Suma_matricial(double [][] MA, double[][] MB){
+        
+        
+    }
+    
+    public static void main(String[] args){
+        
+        Scanner in = new Scanner(System.in);
+        
+        
+        
+        
+        
+        
+    }
+      
+}
