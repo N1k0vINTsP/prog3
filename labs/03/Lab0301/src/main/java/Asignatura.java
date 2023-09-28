414f83dd6b05da8956e7f0a5d1789f202f8fab92
Vince N Perez C <141367327+N1k0vINTsP@users.noreply.github.com>
2023-09-27 23:08:02 -0500

    Add files via upload

--- labs/03/Lab0301/src/main/java/Asignatura.java
+++ labs/03/Lab0301/src/main/java/Asignatura.java
@@ -6,5 +6,55 @@ public class Asignatura {
     int semestre;
     int Num_creditos;
     String horario;
+
+    public Asignatura(int codigo, String Asignatura, int semestre, int Num_creditos, String horario) {
+        this.codigo = codigo;
+        this.Asignatura = Asignatura;
+        this.semestre = semestre;
+        this.Num_creditos = Num_creditos;
+        this.horario = horario;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getAsignatura() {
+        return Asignatura;
+    }
+
+    public void setAsignatura(String Asignatura) {
+        this.Asignatura = Asignatura;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+
+    public int getNum_creditos() {
+        return Num_creditos;
+    }
+
+    public void setNum_creditos(int Num_creditos) {
+        this.Num_creditos = Num_creditos;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+    
+    
     
 }
--- labs/03/Lab0301/src/main/java/Lab0301.java
+++ labs/03/Lab0301/src/main/java/Lab0301.java
@@ -3,32 +3,62 @@ import java.util.Queue;
 
 public class Lab0301 {
     
-    static Queue<Registro> regis = new LinkedList<>();
+    static LinkedList<Asignatura> asignaturas = new LinkedList<>();
+    static Queue<Registro> regis = new LinkedList<>(); //cola para registros de asignaturas por estudiante
+    static Queue<String> codigos = new LinkedList<>();// cola de codigos de los estudiantes
+    
     public static void main(String[] args) {
         
+        carga_BaseDatos_Asignaturas(args[0]);
         registro_estudiante();
         registro_asignaturas();
         reporte_registro_final();
         
     }
     
-    public static void registro_estudiante(){
+    public static void carga_BaseDatos_Asignaturas(String ArchTexto){
+        asignaturas = Entrada.loadFile(ArchTexto);
         
-        String codigo = Entrada.readText("CODIGO ESTUDIANTIL: ");
+    }
+    public static boolean registro_estudiante(){
+        
+        System.out.println(" DATOS PERSONALES ESTUDIANTILES\n\n ");
+        String CodigoEstu = Entrada.readText(" CODIGO ESTUDIANTIL: ");
+        
+        if(CodigoEstu.equals("-")){
+            return false;
+        }
+        
+        if(codigos.contains(CodigoEstu)){
+            System.out.println(" Codigo " + CodigoEstu + " se encuentra en uso \n\n");
+            return true;
+        }
+        
         String nombre = Entrada.readText("NOMRBE DEL ESTUDIANTE: ");
         String Correo = Entrada.readText("CORREO ESTUDIANTE: ");
         int semestre = Entrada.readInt("SEMESTRE: ");
         
-        Estudiante estud = new Estudiante(codigo, nombre, Correo, semestre);
-        //crear registro con el codigo del estudiante (por ahora)
-        //luego agregamos las asignaturas
+        Estudiante estud = new Estudiante(CodigoEstu, nombre, Correo, semestre);
         regis.add(new Registro(estud));
+        codigos.add(CodigoEstu);
+        
+        return true;
     }
     
     public static void registro_asignaturas(){
-        
+         do {
+            int sem = Entrada.readInt("\nVer asignaturas del semestre: ", 1, 10);
+            mostrar_asignaturas(sem);
+        } while (false);
     }
     
+    public static void mostrar_asignaturas(int semestre){
+            for (Asignatura s : asignaturas) {
+            if (s.getSemestre() == semestre)
+                System.out.println(s);
+        }
+        System.out.println();
+    }
     public static void reporte_registro_final(){
         
     }
