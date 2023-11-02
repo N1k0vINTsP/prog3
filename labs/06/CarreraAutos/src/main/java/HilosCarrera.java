e03e3fd739ca9efbb204002c52db024bedbde9f3
Vince N Perez C <141367327+N1k0vINTsP@users.noreply.github.com>
2023-11-02 15:51:49 -0500

    Add files via upload

--- labs/06/CarreraAutos/src/main/java/HilosCarrera.java
+++ labs/06/CarreraAutos/src/main/java/HilosCarrera.java
@@ -1,12 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Estudiante
- */
-public class HilosCarrera {
+import javax.swing.JLabel;
+
+
+public class HilosCarrera extends Thread {
     
+    private JLabel C;
+    private InterfazCarrera IC;
+    
+    public HilosCarrera(JLabel C, InterfazCarrera IC){
+        
+        this.C = C;
+        this.IC = IC;
+    }
+    
+    public void run(){
+        
+        int competidor1 = 0, competidor2 = 0, competidor3 = 0, competidor4 = 0, competidor5 = 0;
+        
+        while (true){
+            try{
+                sleep((int)(Math.random()*1000));
+                competidor1 = IC.getCompetidor1().getLocation().x;
+                competidor2 = IC.getCompetidor2().getLocation().x;
+                competidor3 = IC.getCompetidor3().getLocation().x;
+                competidor4 = IC.getCompetidor4().getLocation().x;
+                competidor5 = IC.getCompetidor5().getLocation().x;
+                
+            
+            }catch (Exception e){
+            }        
+        }
+    }
 }
